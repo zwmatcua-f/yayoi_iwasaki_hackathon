0556c6265d43508d8344d0ca7d32d9c1a63c1b54
zwmatcua-f <111061849+zwmatcua-f@users.noreply.github.com>
2022-11-23 21:02:26 +0900

    dockerfileなおした

--- hackathon/back/main.go
+++ hackathon/back/main.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/joho/godotenv"
 	"log"
 	"net/http"
 	"os"
@@ -24,12 +23,12 @@ var db *sql.DB
 
 func init() {
 
-	err := godotenv.Load(".env_mysql")
-
-	// もし err がnilではないなら、"読み込み出来ませんでした"が出力されます。
-	if err != nil {
-		fmt.Printf("読み込み出来ませんでした: %v", err)
-	}
+	//err := godotenv.Load(".env_mysql")
+	//
+	//// もし err がnilではないなら、"読み込み出来ませんでした"が出力されます。
+	//if err != nil {
+	//	fmt.Printf("読み込み出来ませんでした: %v", err)
+	//}
 
 	mysqlUser := os.Getenv("MYSQL_USER")
 	mysqlPwd := os.Getenv("MYSQL_PWD")
